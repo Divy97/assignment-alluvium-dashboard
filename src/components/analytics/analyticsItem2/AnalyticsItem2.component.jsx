24c5688c07df9180af9b48b9e358790b6dda499f
Divy <divyparekh1810@gmail.com>
2022-12-03 01:37:50 +0530

    item 2 is done

--- src/components/analytics/analyticsItem2/AnalyticsItem2.component.jsx
+++ src/components/analytics/analyticsItem2/AnalyticsItem2.component.jsx
@@ -1,7 +1,51 @@
+import { SlArrowDown } from "react-icons/sl";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 import "./AnalyticsItem2.styles.css";
 
 const AnalyticsItem2 = () => {
-  return <div className="">AnalyticsItem2</div>;
+  const data = {
+    datasets: [
+      {
+        data: [40, 30, 30],
+        backgroundColor: ["red", "#f3f3f3", "darkgray"],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <div className="analyticsItem2_container background">
+      <div className="analyticsItem2_heading background">
+        <span className="para1 background">Idle time causes</span>
+        <span className="para2 background">
+          ALL <SlArrowDown className="background" />
+        </span>
+      </div>
+      <>
+        <div className="analyticsItem2_graph background ">
+          <Doughnut data={data} aspectRatio="2/3" className=" background" />
+          <p className="background">
+            7.5% <br />
+            idle time{" "}
+          </p>
+        </div>
+        <div className="analyticsItem2_stats background">
+          <p className="background para">
+            Missing parts <span className="background span">30%</span>
+          </p>
+          <p className="background para">
+            Services <span className="background span">30%</span>
+          </p>
+          <p className="background para">
+            Broken parts <span className="background span">40%</span>
+          </p>
+        </div>
+      </>
+    </div>
+  );
 };
 
 export default AnalyticsItem2;
